d7de6cf2fe4d8cdd4d25a6b8874788017ca23720
Jonny Chow <jonnyxchow@gmail.com>
2019-11-03 20:24:28 -0500

    Initial Commit

---
+++ PregameApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PregameApp
+//
+//  Created by Jonny Chow on 11/3/19.
+//  Copyright © 2019 Jonny Chow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
